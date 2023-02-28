5d7ea6a73ee12e4f30e55ad7dd5def87287f63bb
datmemerboi <shravanssd@gmail.com>
2023-02-28 18:52:27 +1100

    fix: Config for static HTML export

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['github.com', 'github.githubassets.com']
+    domains: ['github.com', 'github.githubassets.com'],
+    unoptimized: true
   }
 };
 
