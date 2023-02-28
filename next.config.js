3b073d9c8e24e4982e94bdb217e9f40027f27509
datmemerboi <shravanssd@gmail.com>
2023-02-28 19:19:21 +1100

    fix: Try to fix static file import

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
   images: {
     domains: ['github.com', 'github.githubassets.com'],
     unoptimized: true
-  }
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : ''
 };
 
 module.exports = nextConfig;
